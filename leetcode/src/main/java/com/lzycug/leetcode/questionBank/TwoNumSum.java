3018ae5560b206c1ca609818c446d8f85a00c6a4
Lizhiyang <lzycug@126.com>
2020-02-28 20:37:02 +0800

    update leetcode

--- leetcode/src/main/java/com/lzycug/leetcode/questionBank/TwoNumSum.java
+++ leetcode/src/main/java/com/lzycug/leetcode/questionBank/TwoNumSum.java
@@ -1,6 +1,9 @@
 
 package com.lzycug.leetcode.questionBank;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
  * 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
@@ -16,7 +19,7 @@ public class TwoNumSum {
     public static void main(String[] args) {
         int[] nums = {-2, 7, -11, 15};
         int target = -4;
-        int[] subscript = twoSum(nums, target);
+        int[] subscript = twoSum1(nums, target);
         System.out.println("[" + subscript[0] + ", " + subscript[1] + "]");
     }
 
@@ -27,7 +30,7 @@ public class TwoNumSum {
      * @param target 目标值
      * @return 数组下标的数组
      */
-    public static int[] twoSum(int[] nums, int target) {
+    public static int[] twoSum1(int[] nums, int target) {
         int[] array = new int[2];
         if (nums.length < 2) {
             return array;
@@ -52,5 +55,17 @@ public class TwoNumSum {
             }
             return array;
         }
+    }
+
+    public static int[] twoSum2(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] {map.get(complement), i};
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
     }
 }
